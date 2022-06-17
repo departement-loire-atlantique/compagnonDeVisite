394ca360985115ccfe1ff32d35c6270a5b0f1633
paul604 <paul604@users.noreply.github.com>
2022-06-17 15:04:17 +0200

    languages list
    TODO use list component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, LOCALE_ID, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -7,11 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  private _nextPageRoute: string = "/TODO";
+
+  languages = [
+    { lbl: 'FR', url: '/fr' + this._nextPageRoute },
+    { lbl: 'EN', url: '/en' + this._nextPageRoute },
+    { lbl: 'LSF', url: '/fr-LSF' + this._nextPageRoute }
+  ];
+
+  constructor(@Inject(LOCALE_ID) public locale: string) {
+    if (!environment.production) {
+      for (let lang of this.languages) {
+        lang.url = this._nextPageRoute;
+      }
+    }
+  }
 
   ngOnInit(): void {
     console.log('TODO Home');
-
+    console.log(this.locale);
   }
 
 }
