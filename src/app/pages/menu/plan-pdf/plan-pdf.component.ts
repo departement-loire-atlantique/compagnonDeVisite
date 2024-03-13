3b3fe67074ea532e1be2fc30346cf068f7799714
Fabien Debiais <fabien.debiais@digiwin.fr>
2024-03-13 13:13:47 +0100

    Menu burger: planPDF by category

--- src/app/pages/menu/plan-pdf/plan-pdf.component.ts
+++ src/app/pages/menu/plan-pdf/plan-pdf.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
-import { JAngularService } from 'j-angular';
+import { JAngularService,JcmsPager } from 'j-angular';
 import { Media } from 'src/app/models/jcms/media';
-import { buildUrlMedia } from 'src/app/models/jcms/content';
+import { buildUrlMedia, Content } from 'src/app/models/jcms/content';
+import { Observable } from 'rxjs';
+import { JcmsEspace } from 'src/app/models/environment';
+import { EspaceByLangService } from 'src/app/services/espace-by-lang.service';
 
 import panzoom from "panzoom";
 
@@ -12,22 +15,23 @@ import panzoom from "panzoom";
 })
 export class PlanPDFComponent implements OnInit, AfterViewInit {
   @ViewChild('imagePlan', { static: false }) imagePlan!: ElementRef;
-
+  pager: JcmsPager<Content> | undefined;
+  espaceJcms: JcmsEspace | undefined;
+  idCatHome: string = '';
   planPDF: String = '';
   title: string = $localize`:@@OverlayMapComp-titre:Carte`;
   closeTxt = $localize `:@@OverlayMapComp-close:Fermer la boîte de dialogue \: ${this.title}:title:`
   idTarget: string = "overlay-map";
 
-  constructor( private _jcms: JAngularService) { }
+  constructor( private _jcms: JAngularService, private _jcmsEspace: EspaceByLangService, ) {
+    this.espaceJcms = this._jcmsEspace.getJcmsSpace();
+    if (this.espaceJcms) {
+      this.idCatHome = this.espaceJcms.catHome;
+    }
+  }
 
   ngOnInit(): void {
-    this._jcms.get<any>('admin/property/jcmsplugin.compagnonDeVisite.planPDF').subscribe((result: any) => {
-        this._jcms.get<Media>('data/' + result.value).subscribe((media: Media) => {
-          if (media) {
-            this.planPDF = buildUrlMedia(media.filename);
-          }
-        });
-    });
+   this.research();
   }
 
   ngAfterViewInit() {
@@ -36,21 +40,64 @@ export class PlanPDFComponent implements OnInit, AfterViewInit {
       minZoom: 0.5
     });
   }
-    /**
+
+  /**
+   * Stockage du résultat
+   * @param obs
+   */
+  public processResult(obs: Observable<JcmsPager<Content>>) {
+    obs.subscribe((pager: JcmsPager<Content>) => {
+
+      this.pager = pager;
+      const contents = pager.dataInPage;
+
+      const itContent = contents[0];
+      console.log("resf " + contents);
+
+      this._jcms.get<Media>('data/' + itContent.id).subscribe(res => {
+        this.planPDF = buildUrlMedia(res.filename)
+      });
+    });
+  }
+
+  /**
+   * Lance la recherche du planPDF
+   * @returns
+   */
+  public research(): void {
+    this.processResult(
+      this._jcms.getPager<Content>('search', {
+        params: {
+          types: ['Media'],
+          exactType: true,
+          exactCat: true,
+          mode: 'advanced',
+          wrkspc: this.espaceJcms ? this.espaceJcms.espace : "",
+          cids: this.idCatHome,
+        },
+      })
+    );
+  }
+
+  /**
    * get label bouton
    */
-    public getLabelBtn() {
-      return $localize`:@@BackComp-text:Retour` ;
-    }
+  public getLabelBtn() {
+    return $localize`:@@BackComp-text:Retour` ;
+  }
 
-    public zoom(palier: number = 100) {
-      var currWidth = this.imagePlan.nativeElement.clientWidth;
-      if (currWidth < 300) {
-        currWidth = 300;
-      } else if (currWidth > 700) {
-        currWidth = 700;
-      }
-        this.imagePlan.nativeElement.style.width = (currWidth - palier) + "px"
-        this.imagePlan.nativeElement.style.objectFit = "cover";
+  /**
+   * Gestion du zoom
+   * @param palier
+   */
+  public zoom(palier: number = 100) {
+    var currWidth = this.imagePlan.nativeElement.clientWidth;
+    if (currWidth < 300) {
+      currWidth = 300;
+    } else if (currWidth > 700) {
+      currWidth = 700;
     }
+    this.imagePlan.nativeElement.style.width = (currWidth - palier) + "px"
+    this.imagePlan.nativeElement.style.objectFit = "cover";
+  }
 }
